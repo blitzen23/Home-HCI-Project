04b5d2973186d45e6bfa7c20db3507c4b094f010
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2021-06-21 14:57:49 +0700

    Slide Update

--- script/home.js
+++ script/home.js
@@ -13,149 +13,7 @@ $(() =>{
         "color": "white"
     })
 
-    var leftSlideFunction = function(){
-        $('.slider-inner').animate({left: + perSliderItemWidth}, 1000, function(){
-            $('.slider-item:last-child').prependTo('.slider-inner')
-            $('.slider-inner').css({left: 0})
-        })
-        if (ctr == 1){
-            $('.slider-icon:nth-child(1)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 2){
-            $('.slider-icon:nth-child(2)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 3){
-            $('.slider-icon:nth-child(3)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 4){
-            $('.slider-icon:nth-child(4)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 5){
-            $('.slider-icon:nth-child(5)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        ctr -= 1
-        if (ctr <= 0){
-            ctr = 5
-        }
-        if (ctr == 1){
-            $('.slider-icon:nth-child(1)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 2){
-            $('.slider-icon:nth-child(2)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 3){
-            $('.slider-icon:nth-child(3)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 4){
-            $('.slider-icon:nth-child(4)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 5){
-            $('.slider-icon:nth-child(5)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-    }
-
-    var rightSlideFunction = function(){
-        $('.slider-inner').animate({left: - perSliderItemWidth}, 1000, function(){
-            $('.slider-item:first-child').appendTo('.slider-inner')
-            $('.slider-inner').css({left: 0})
-        })
-        if (ctr == 1){
-            $('.slider-icon:nth-child(1)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 2){
-            $('.slider-icon:nth-child(2)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 3){
-            $('.slider-icon:nth-child(3)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 4){
-            $('.slider-icon:nth-child(4)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        else if (ctr == 5){
-            $('.slider-icon:nth-child(5)').css({
-                "opacity": "100%",
-                "color": "gray"
-            })
-        }
-        ctr += 1
-        if (ctr > 5){
-            ctr = 1
-        }
-        if (ctr == 1){
-            $('.slider-icon:nth-child(1)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 2){
-            $('.slider-icon:nth-child(2)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 3){
-            $('.slider-icon:nth-child(3)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 4){
-            $('.slider-icon:nth-child(4)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-        else if (ctr == 5){
-            $('.slider-icon:nth-child(5)').css({
-                "opacity": "75%",
-                "color": "white"
-            })
-        }
-    }
-
-    var rightSlideIcon = function(count){
+     function rightSlideFunction(count){
         $('.slider-inner').animate({left: - perSliderItemWidth * count}, 1000, function(){
             for(let i = 0; i < count; i++){
                 $('.slider-item:first-child').appendTo('.slider-inner')
@@ -305,105 +163,40 @@ $(() =>{
 
     }
 
-    $('#left-arrow').click(leftSlideFunction)
+    function iconSlideDirection(count){
+        if(ctr < count){
+            rightSlideFunction(count - ctr)
+        } else if(ctr > count){
+            leftSlideFunction(ctr - count)
+        }
+    }
 
-    $('#right-arrow').click(rightSlideFunction)
+    $('#left-arrow').click(()=>{
+        leftSlideFunction(1)
+    })
+
+    $('#right-arrow').click(()=>{
+        rightSlideFunction(1)
+    })
+
 
     $('#slider-icon-1').click(function(){
-        let tempCtr = 1 
-        let count = 0
-        while(tempCtr != ctr){
-            tempCtr = (tempCtr % 5) + 1;
-            count++;
-        }
-
-        if(count != 0){
-            if(count <= 2){
-                leftSlideIcon(count)
-            } else {
-                count = 5 - count
-                rightSlideIcon(count)
-            }
-
-        }
-
+        iconSlideDirection(1)
     })
 
     $('#slider-icon-2').click(function(){
-        let tempCtr = 2
-        let count = 0
-        while(tempCtr != ctr){
-            tempCtr = (tempCtr % 5) + 1;
-            count++;
-        }
-
-        if(count != 0){
-            if(count <= 2){
-                leftSlideIcon(count)
-            } else {
-                count = 5 - count
-                rightSlideIcon(count)
-            }
-
-        }
-
+        iconSlideDirection(2)
     })
 
     $('#slider-icon-3').click(function(){
-        let tempCtr = 3
-        let count = 0
-        while(tempCtr != ctr){
-            tempCtr = (tempCtr % 5) + 1;
-            count++;
-        }
-
-        if(count != 0){
-            if(count <= 2){
-                leftSlideIcon(count)
-            } else {
-                count = 5 - count
-                rightSlideIcon(count)
-            }
-
-        }
-
+        iconSlideDirection(3)
     })
 
     $('#slider-icon-4').click(function(){
-        let tempCtr = 4
-        let count = 0
-        while(tempCtr != ctr){
-            tempCtr = (tempCtr % 5) + 1;
-            count++;
-        }
-
-        if(count != 0){
-            if(count <= 2){
-                leftSlideIcon(count)
-            } else {
-                count = 5 - count
-                rightSlideIcon(count)
-            }
-        }
+        iconSlideDirection(4)
     })
 
     $('#slider-icon-5').click(function(){
-        let tempCtr = 5
-        let count = 0
-        while(tempCtr != ctr){
-            tempCtr = (tempCtr % 5) + 1;
-            count++;
-        }
-
-        if(count != 0){
-            if(count <= 2){
-                leftSlideIcon(count)
-            } else {
-                count = 5 - count
-                rightSlideIcon(count)
-            }
-
-        }
-
+        iconSlideDirection(5)
     })
 })
